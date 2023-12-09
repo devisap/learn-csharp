55421b164ebcd8700f2d0c29a7c7006cf0dad321
devisap <ilham.sagitaputra@gmail.com>
2023-12-09 08:54:26 +0700

    Add project files.

---
+++ LearnBasicC#/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnBasicC_
+{
+    class App
+    {
+        public int Counting = 20;
+        private int _okeDokey = 15;
+        private const int HALO_BRODYE = 12;
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Ilham Sagita Putra");
+        }
+    }
+}
